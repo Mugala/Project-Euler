221e071285d14d10a01b9d239e58f2aea5e08c36
mugala <mugalalevi21@gmail.com>
2018-05-02 22:44:37 +0300

    This commit will solve project euler problem 10

--- prob_10.py
+++ prob_10.py
@@ -1,10 +1,26 @@
 
-#Function to test and see if a number is a prime number
+#Function to test and see if a number is a prime )_
 def is_prime(n):
-    for i in range (2,n):
+    for i in range (2,int(n**0.5)+1):
         if n % i == 0:
             return False 
         
     return True 
 
-print(is_prime(10))
+#Function to create a list and loop to test if number is prime
+
+def test():
+    list_prime = []
+    for x in range(2,2000000):
+        if is_prime(x) == True:
+            list_prime.append(x)
+    p = sum(list_prime)
+
+    print(p)
+
+
+print(test())
+
+
+
+
